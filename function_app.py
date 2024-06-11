5fd7f423713903083ba97082f0ca67a97c4959d8
ergz <erodriguez@flowwest.com>
2024-06-11 15:42:47 -0700

    wip - able to send data to the data folder from the db

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import azure.functions as func
 import datetime
 import json
@@ -7,11 +8,11 @@ from sqlalchemy import create_engine, Engine, text
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, PublicAccess
 
 logger = logging.getLogger(__name__)
 DB_NAME = os.getenv("DB_NAME") or "runiddb"
-DB_URL = os.getenv("DB_URL") or "localhost"
+DB_HOST = os.getenv("DB_HOST") or "localhost"
 DB_USER = os.getenv("DB_USER") or "emanuel"
 DB_PASSWORD = os.getenv("DB_PASSWORD") or "superpassword"
 DB_PORT = os.getenv("DB_PORT") or 5432
@@ -22,9 +23,6 @@ BASE_URLS = {
     "production": "https://pasta.lternet.edu/",
 }
 
-conn_string = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_URL}:{DB_PORT}/{DB_NAME}"
-)
 
 app = func.FunctionApp()
 
@@ -59,21 +57,49 @@ def get_latest_genetics_results(engine: Engine):
     return data
 
 
-class pastaPackage:
-    def __init__(self, container, conn_string, server="staging"):
-        self.container = container
-        self.conn_string = conn_string
+class EDIPipeline:
+    """
+    EDI Pipeline
+
+    - xml_container: the blob container where metadata xml files are stored
+    - server: the edi server to use for data uploads
+    """
+
+    def __init__(
+        self,
+        az_blob_container: str,
+        server="staging",
+    ):
+        self.az_blob_container = az_blob_container
         self._base_url = BASE_URLS.get(server)
-        self.blob_service_client = BlobServiceClient.from_connection_string(
-            self.conn_string
+
+        # self._get_latest_xml_url()
+        # self._make_soup()
+
+    def az_blob_connect(self, conn_str: str):
+        self.blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+        container_client = self.blob_service_client.get_container_client(
+            self.az_blob_container
         )
-        self._get_latest_xml_url()
-        self._make_soup()
+        if not container_client.exists():
+            logging.info("container not found, creating from template...")
+            container_client.create_container(public_access=PublicAccess.CONTAINER)
+            init_files = ["xml/init.txt", "data/init.txt"]
+            for file in init_files:
+                container_client.upload_blob(file, b"")
+            logging.info("creation succesfull")
+
+        self.container_client = container_client
+
+    def db_connect(self, dbname, user, password, host, port):
+        db_conn_string = (
+            f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+        )
+        self.engine = create_engine(db_conn_string)
 
     def _get_package_xmls(self):
         # TODO: get this from the env/az app settings
-        container_client = self.blob_service_client.get_container_client(self.container)
-        blob_list = list(container_client.list_blobs())
+        blob_list = list(self.container_client.list_blobs(name_starts_with="xml/"))
 
         if len(blob_list) == 0:
             return None
@@ -83,7 +109,7 @@ class pastaPackage:
     def _get_latest_xml_url(self):
         latest = self._get_package_xmls()
         if latest:
-            blob_url = f"https://{self.blob_service_client.account_name}.blob.core.windows.net/{self.container}/{latest[0].get('name')}"
+            blob_url = f"https://{self.blob_service_client.account_name}.blob.core.windows.net/{self.az_blob_container}/{latest[0].get('name')}"
             self.latest_url = blob_url
         else:
             return None
@@ -114,33 +140,77 @@ class pastaPackage:
 
     def write_xml_to_blob(self):
         current_id = self.get_package_id()
-        filename = f"{current_id}.xml"
+        filename = f"xml/{current_id}.xml"
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container, blob=filename
+            container=self.az_blob_container, blob=filename
         )
         xml_content = str(self.soup)
         blob_client.upload_blob(xml_content, overwrite=True)
 
+    def write_csv_to_blob(self, filename: str, data: pd.DataFrame):
+        blob_client = self.blob_service_client.get_blob_client(
+            container=self.az_blob_container, blob=f"data/{filename}.csv"
+        )
+        csv_binary = StringIO()
+        data.to_csv(csv_binary, index=False)
+        csv_content = csv_binary.getvalue()
 
-class simpleEML:
-    def __init__(self, url):
-        self.url = url
-        resp = requests.get(self.url)
-        self.soup = BeautifulSoup(resp.content, "lxml-xml")
+        blob_client.upload_blob(csv_content)
+
+    def get_latest_genetics_data(self):
+        logging.info("attempting to retrieve latest genetics data")
+
+        q = """
+            SELECT DISTINCT ON (gri.sample_id)
+                gri.sample_id,
+                rt.run_name,
+                substring(gri.sample_id FROM '^[^_]+_((?:100|[1-9][0-9]?))_') AS sample_event,
+                st.datetime_collected,
+                st.fork_length_mm,
+                st.field_run_type_id
+            FROM
+                genetic_run_identification gri
+            JOIN
+                public.run_type rt
+            ON
+                rt.id = gri.run_type_id
+            JOIN
+                public.sample st
+            ON st.id = gri.sample_id
+            WHERE
+                gri.sample_id LIKE '___24%%'
+            ORDER BY
+                gri.sample_id,
+                gri.created_at DESC;
+        """
+        with self.engine.connect() as conn:
+            data = pd.read_sql_query(q, conn)
+        return data
+
+    def latest_data_db_to_blob(self):
+        """
+        perform all operations to get the latest data from database and upload to blob
+        """
+        latest_data = self.get_latest_genetics_data()
+        new_package_id = self.package_id_revision_increment()
+        filename = f"genetics-resuts-{new_package_id}.csv"
+        self.write_csv_to_blob(filename)
+
+    def latest_xml_to_blob(self):
+        self.update_package_id_tag()
+        self.write_xml_to_blob()
 
 
 @app.function_name(name="genetics-EDI-processing")
 @app.route(route="fetch-data")
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("reading the base xml file")
-    conn_string = os.getenv("AZURE_BLOB_CONN_STRING") or "dev-string"
-    p = pastePackage("edi-1617", conn_string)
+    conn_string = os.getenv("AZURE_BLOB_CONN_STRING") or ""
+    p = EDIPipeline("edi-1617")
+    p.az_blob_connect(conn_string)
+    p.db_connect(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
+    data = p.get_latest_genetics_data()
+    p.write_csv_to_blob("genetics-data", data)
+    # # p.latest_data_db_to_blob()
+    # p.latest_xml_to_blob()
 
-    logging.info("base xml read in")
-    logging.info("This is a test")
-    engine = create_engine(conn_string)
-
-    data = get_latest_genetics_results(engine)
-    logging.info(data)
-
-    return func.HttpResponse("test is good")
+    return func.HttpResponse("EDI Pipeline Excecution Complete")
