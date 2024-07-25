b0ff3fc15167832149186d172e8e08b763ef912a
ergz <erodriguez@flowwest.com>
2024-07-25 14:18:23 -0700

    remove old code commented out and function now runs from start to end
    what still needs to happen is that there needs to be the connection to
    edi pasta api

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ from xml.etree.ElementTree import XML, XMLParser
 import azure.functions as func
 import logging
 import os
-from sqlalchemy import create_engine, Engine, text
+from sqlalchemy import create_engine, Engine, over, text
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
@@ -77,6 +77,9 @@ def read_sql_from_file(file_name: str):
         return f.read()
 
 
+def read_sql_from_blob(blob_path: str): ...
+
+
 def get_latest_data(db: Optional[Engine], query_statement: str):
     if db is None:
         raise Exception(
@@ -88,14 +91,14 @@ def get_latest_data(db: Optional[Engine], query_statement: str):
         return data
 
 
-def upload_csv_to_blob(blob_prefix, blob_service_client, filename, data):
-    blob_client = blob_service_client.get_blob_client(
-        container=blob_prefix, blob=f"data/{filename}.csv"
-    )
+def upload_csv_to_blob(
+    blob_prefix, blob_service_client, filename, data, overwrite=False
+):
+    blob_client = blob_service_client.get_blob_client(blob=f"data/{filename}.csv")
     csv_binary = StringIO()
     data.to_csv(csv_binary, index=False)
     csv_content = csv_binary.getvalue()
-    blob_client.upload_blob(csv_content)
+    blob_client.upload_blob(csv_content, overwrite=overwrite)
     return blob_client.url
 
 
@@ -150,24 +153,31 @@ def package_id_revision_increment(self):
         return f"{package_id_parts[0]}.{package_id_parts[1]}.{new_revision_number}"
 
 
-def update_package_id_tag(self):
+def update_package_id_tag(self) -> None:
     new_package_id = self.package_id_revision_increment()
     eml_tag = self.soup.find("eml:eml")
     if eml_tag:
         eml_tag["packageId"] = new_package_id
 
 
-def write_xml_to_blob(
-    xml: BeautifulSoup, package_number: str, container_client: ContainerClient
-):
+def increment_package_revision_number(id: str) -> str:
+    split_id = id.split(".")
+    revision = int(split_id[-1]) + 1
+    split_id[-1] = str(revision)
+    return ".".join(split_id)
+
+
+def write_xml_to_blob(xml: BeautifulSoup, container_client: ContainerClient) -> str:
     eml_tag = xml.find("eml:eml")
-    package_id = eml_tag["packageId"]
+    if eml_tag is None:
+        return None
+    package_id = eml_tag.get("packageId")
     filename = f"xml/{package_id}.xml"
-    blob_client = container_client.get_blob_client(
-        container=package_number, blob=filename
-    )
+    blob_client = container_client.get_blob_client(filename)
     xml_content = str(xml)
     blob_client.upload_blob(xml_content, overwrite=True)
+
+    return get_url_for_xml(filename, container_client)
 
 
 # set up the container and azure blob
@@ -186,156 +196,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     q = read_sql_from_file("data-query.sql")
     data = get_latest_data(pipe.db_engine, q)
     new_url = upload_csv_to_blob(
-        pipe.pkg_number, pipe.container_client, "genetics-data", data
+        pipe.pkg_number, pipe.container_client, "genetics-data", data, overwrite=True
     )
 
     xmls = get_package_xmls(pipe.container_client, sort=True)
-    xml_url = get_url_for_xml(xmls[0].name)
+    xml_url = get_url_for_xml(xmls[0].name, pipe.container_client)
     xml_soup = parse_xml_from_url(xml_url)
 
     update_eml(xml_soup, {EML_PATHS["csv_url"]: new_url})
 
-    write_xml_to_blob(xml_soup, pipe.container_client)
-
     return func.HttpResponse("EDI Pipeline Excecution Complete")
-
-
-# class EDIPipeline:
-#     """
-#     EDI Pipeline
-#     - xml_container: the blob container where metadata xml files are stored
-#     - server: the edi server to use for data uploads
-#     """
-#
-#     def __init__(
-#         self,
-#         az_blob_container: str,
-#         server="staging",
-#     ):
-#         self.az_blob_container = az_blob_container
-#         self._base_url = BASE_URLS.get(server)
-#         self.query_file = "data-query.sql"
-#
-#         # self._get_latest_xml_url()
-#         # self._make_soup()
-#
-#     def az_blob_connect(self, conn_str: str):
-#         self.blob_service_client = BlobServiceClient.from_connection_string(conn_str)
-#         container_client = self.blob_service_client.get_container_client(
-#             self.az_blob_container
-#         )
-#
-#         # create bucket if it doesn't exist alredy and add temp folders in each
-#         if not container_client.exists():
-#             logging.info("container not found, creating from template...")
-#             container_client.create_container(public_access=PublicAccess.CONTAINER)
-#             init_files = ["xml/init.txt", "data/init.txt"]
-#             for file in init_files:
-#                 container_client.upload_blob(file, b"")
-#             logging.info("creation succesfull")
-#
-#         self.container_client = container_client
-#
-#     def db_connect(
-#         self,
-#         dbname=os.getenv("DB_NAME"),
-#         user=os.getenv("DB_USER"),
-#         password=os.getenv("DB_PASSWORD"),
-#         host=os.getenv("DB_HOST"),
-#         port=os.getenv("DB_PORT"),
-#     ):
-#         db_conn_string = (
-#             f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
-#         )
-#         self.engine = create_engine(db_conn_string)
-#
-#     def _get_package_xmls(self):
-#         # TODO: get this from the env/az app settings
-#         blob_list = list(self.container_client.list_blobs(name_starts_with="xml/"))
-#
-#         if len(blob_list) == 0:
-#             return None
-#
-#         return sorted(blob_list, key=lambda x: x["last_modified"], reverse=True)
-#
-#     def _get_latest_xml_url(self):
-#         all_xmls = self._get_package_xmls()
-#         if all_xmls:
-#             latest = all_xmls[0]
-#             blob_client = self.container_client.get_blob_client(latest.name)
-#             self.latest_url = blob_client.url
-#             return blob_client.url
-#         else:
-#             return None
-#
-#     def _make_soup(self):
-#         resp = requests.get(self.latest_url)
-#         content = resp.content
-#         soup = BeautifulSoup(content, "lxml-xml")
-#         self.soup = soup
-#
-#     def get_package_id(self):
-#         eml_tag = self.soup.find("eml:eml")
-#         if eml_tag is not None:
-#             return eml_tag["packageId"]
-#
-#     def package_id_revision_increment(self):
-#         current_package_id = self.get_package_id()
-#         if current_package_id:
-#             package_id_parts = current_package_id.split(".")
-#             new_revision_number = int(package_id_parts[2]) + 1
-#             return f"{package_id_parts[0]}.{package_id_parts[1]}.{new_revision_number}"
-#
-#     def update_package_id_tag(self):
-#         new_package_id = self.package_id_revision_increment()
-#         eml_tag = self.soup.find("eml:eml")
-#         if eml_tag:
-#             eml_tag["packageId"] = new_package_id
-#
-#     def write_xml_to_blob(self):
-#         current_id = self.get_package_id()
-#         filename = f"xml/{current_id}.xml"
-#         blob_client = self.blob_service_client.get_blob_client(
-#             container=self.az_blob_container, blob=filename
-#         )
-#         xml_content = str(self.soup)
-#         blob_client.upload_blob(xml_content, overwrite=True)
-#
-#     def write_csv_to_blob(self, filename: str, data: pd.DataFrame):
-#         blob_client = self.blob_service_client.get_blob_client(
-#             container=self.az_blob_container, blob=f"data/{filename}.csv"
-#         )
-#         csv_binary = StringIO()
-#         data.to_csv(csv_binary, index=False)
-#         csv_content = csv_binary.getvalue()
-#
-#         blob_client.upload_blob(csv_content)
-#
-#         return blob_client.url
-#
-#     def read_sql_file(self, file_name: str):
-#         with open(file_name, "r") as f:
-#             return f.read()
-#
-#     def set_query_file(self, file_name: str):
-#         self.query_file = file_name
-#
-#     def get_latest_data(self):
-#         query_statement = self.read_sql_file(file_name=self.query_file)
-#
-#         with self.engine.connect() as conn:
-#             data = pd.read_sql_query(query_statement, conn)
-#         return data
-#
-#     def latest_xml_to_blob(self):
-#         self.update_package_id_tag()
-#         self.write_xml_to_blob()
-#
-#     def update_package_metadata(
-#         self, base_xml_url: str, overwrite_values: dict[str, str]
-#     ):
-#         self._get_latest_xml_url()
-#         self._make_soup()
-#
-#         overwrite_values = overwrite_values
-#
+    write_xml_to_blob(xml_soup, pipe.container_client)
